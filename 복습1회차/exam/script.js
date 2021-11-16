fa0646bbbdbb0916595ca7d9daed6c4ee8f8563d
yonggyo1981 <yonggyo1981@gmail.com>
2021-11-16 13:23:13 +0900

    .

---
+++ 복습1회차/exam/script.js
@@ -0,0 +1,9 @@
+/*
+window.addEventListener("DOMContentLoaded", function() {
+	const button = document.getElementById("button");
+	console.log("button:", button);
+});
+*/
+
+const button = document.getElementById("button");
+console.log("button:", button);
